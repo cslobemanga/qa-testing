eee8f827e87759e10028e9990dbfacf88117e5bc
Charles Lobe-Manga <charles.lobe-manga@meetago.com>
2019-05-22 11:51:20 +0200

    Adding class ChangeBudget

---
+++ testing/src/main/java/com/meetago/qa/testing/ChangeBudget.java
@@ -0,0 +1,13 @@
+package com.meetago.qa.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class ChangeBudget 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
